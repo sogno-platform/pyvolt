148a631f3a11fc69346869673a993a6b4308a9b3
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2018-10-18 14:51:22 +0200

    restructure package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ACS-State-Estimation',
+      version='0.1',
+      description='State Estimation',
+      author='Marco Pau',
+      author_email='mpau@eonerc.rwth-aachen.de',
+     )
