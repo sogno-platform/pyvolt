f62b5eb37378a0242ed33410868ff1c39f3aec9c
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2018-11-05 16:47:16 +0100

    remove unecessary code from pf test

--- acs-state-estimation/test_powerflow.py
+++ acs-state-estimation/test_powerflow.py
@@ -3,8 +3,6 @@ import math
 import matplotlib as plt
 import Network_data
 import powerflow
-import Meas_data
-import StateEstimator
 
 class PerUnit:
     def __init__(self, S, V):
@@ -12,35 +10,6 @@ class PerUnit:
         self.V = V
         self.I = S/V
         self.Z = S/(V**2)
-        
-class Measurements:
-    def __init__(self, index, unc):
-        self.index = index.astype(int)
-        self.unc = unc
-        self.num = len(index)
-        
-class Measurement_set:
-    def __init__(self, V, I, Sinj, S1, S2, Vpmu_mag, Vpmu_phase, Ipmu_mag, Ipmu_phase):
-        self.V = V
-        self.Sinj = Sinj
-        self.S1 = S1
-        self.S2 = S2
-        self.I = I
-        self.Vpmu_mag = Vpmu_mag
-        self.Vpmu_phase = Vpmu_phase 
-        self.Ipmu_mag = Ipmu_mag
-        self.Ipmu_phase = Ipmu_phase
-        
-class Zdata_init:
-    def __init__(self, meas):
-        nmeas = meas.V.num + meas.I.num + 2*meas.Sinj.num + 2*meas.S1.num + 2*meas.S2.num + meas.Ipmu_mag.num + meas.Ipmu_phase.num + meas.Vpmu_mag.num + meas.Vpmu_phase.num
-        self.mtype = numpy.zeros(nmeas)
-        self.mval = numpy.zeros(nmeas)
-        self.mmeas = numpy.zeros(nmeas)
-        self.mbranch = numpy.zeros(nmeas)
-        self.mfrom = numpy.zeros(nmeas)
-        self.mto = numpy.zeros(nmeas)
-        self.mstddev = numpy.zeros(nmeas)
         
 """ Insert here per unit values of the grid for power and voltage """
 S = 100*(10**6)
