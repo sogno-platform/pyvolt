de27f84e3fe4ddc978d36f0f6c625ff2d5637c32
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2018-11-08 16:45:41 +0100

    update setup

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ from distutils.core import setup
 setup(name='acs-state-estimation',
       version='0.1',
       description='State Estimation',
-      author='Marco Pau',
-      author_email='mpau@eonerc.rwth-aachen.de',
-     )
+      author='Marco Pau, Markus Mirz',
+      author_email='acs-software@eonerc.rwth-aachen.de',
+      packages = [ 'acs.state_estimation' ],
+      install_requires = [
+        'matplotlib',
+        'numpy',
+      ]
+)
