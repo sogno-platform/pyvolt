19b5ecbc00d2c638675daef2533d30acc311fdd5
Jan Dinkelbach <jdinkelbach@eonerc.rwth-aachen.de>
2019-10-31 15:25:51 +0100

    started to adapt for usage of new cimpy implementation

--- examples/quickstart/run_nv_powerflow.py
+++ examples/quickstart/run_nv_powerflow.py
@@ -9,14 +9,18 @@ os.chdir(os.path.dirname(__file__))
 
 logging.basicConfig(filename='CIGRE.log', level=logging.INFO, filemode='w')
 
-cim_xml_path = r".\sample_data"
-cim_xml_files = [cim_xml_path + r"\Rootnet_FULL_NE_06J16h_DI.xml",
-                 cim_xml_path + r"\Rootnet_FULL_NE_06J16h_EQ.xml",
-                 cim_xml_path + r"\Rootnet_FULL_NE_06J16h_SV.xml",
-                 cim_xml_path + r"\Rootnet_FULL_NE_06J16h_TP.xml"]
+xml_path = r".\sample_data"
+xml_files = [xml_path + r"\Rootnet_FULL_NE_06J16h_DI.xml",
+                 xml_path + r"\Rootnet_FULL_NE_06J16h_EQ.xml",
+                 xml_path + r"\Rootnet_FULL_NE_06J16h_SV.xml",
+                 xml_path + r"\Rootnet_FULL_NE_06J16h_TP.xml"]
+
+xml_files_abs = []
+for file in xml_files:
+    xml_files_abs.append(os.path.abspath(file))
 
 # read cim files and create new network.Systen object
-res = cimpy.cimread(cim_xml_files)
+res, _ = cimpy.cim_import(xml_files_abs, "cimgen_v2_4_15")
 system = network.System()
 base_apparent_power = 25  # MW
 system.load_cim_data(res, base_apparent_power)
