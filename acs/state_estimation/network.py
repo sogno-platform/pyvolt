d437ea4447b2bd7ff81ee2cb9a95b259a412acb9
martin.moraga <martin.moraga.91@gmail.com>
2019-11-07 13:34:30 +0100

    use mRIDs instead names

--- acs/state_estimation/network.py
+++ acs/state_estimation/network.py
@@ -73,49 +73,40 @@ class System():
         list_TPNode = [elem for elem in res.values() if elem.__class__.__name__ == "TopologicalNode"]
         list_SvVoltage = [elem for elem in res.values() if elem.__class__.__name__ == "SvVoltage"]
         list_SvPowerFlow = [elem for elem in res.values() if elem.__class__.__name__ == "SvPowerFlow"]
-        #list_EnergyConsumer = [elem for elem in res.values() if elem.__class__.__name__ == "EnergyConsumer"]
         list_ACLineSegment = [elem for elem in res.values() if elem.__class__.__name__ == "ACLineSegment"]
         list_PowerTransformer = [elem for elem in res.values() if elem.__class__.__name__ == "PowerTransformer"]
         list_Terminals = [elem for elem in res.values() if elem.__class__.__name__ == "Terminal"]
-        #list_Terminals_EC = [elem for elem in list_Terminals if elem.ConductingEquipment[0].__class__.__name__ == "EnergyConsumer"]
         list_PowerTransformerEnds = [elem for elem in res.values() if elem.__class__.__name__ == "PowerTransformerEnd"]
-
+  
         #create nodes
         for TPNode in list_TPNode:
-            name_TPNode = TPNode.name
+            uuid_TPNode = TPNode.mRID
             vmag = 0.0
             vphase = 0.0
             pInj = 0.0
             qinj = 0.0
                 
             for obj_SvVoltage in list_SvVoltage:
-                if obj_SvVoltage.TopologicalNode[0].name == name_TPNode:
+                if obj_SvVoltage.TopologicalNode[0].mRID == uuid_TPNode:
                     vmag = obj_SvVoltage.v
                     vphase = obj_SvVoltage.angle
                     break
 
             for obj_SvPowerFlow in list_SvPowerFlow:
-                #TODO: gibt es nur ein SvPowerFlow pro Knoten??
-                if obj_SvPowerFlow.Terminal[0].TopologicalNode[0].name == name_TPNode:
+                if obj_SvPowerFlow.Terminal[0].TopologicalNode[0].mRID == uuid_TPNode:
                     pInj += obj_SvPowerFlow.p
                     qinj += obj_SvPowerFlow.q                
         
-            #for obj_Terminal in list_Terminals_EC:
-            #    #TODO: is it correct?
-            #    if obj_Terminal.TopologicalNode[0].name == name_TPNode:
-            #        energy_consumer_name = obj_Terminal.ConductingEquipment[0].name
-
-
             base_voltage = TPNode.BaseVoltage[0].nominalVoltage
-            self.nodes.append(Node(uuid=name_TPNode, base_voltage=base_voltage, v_mag=vmag,
+            self.nodes.append(Node(uuid=uuid_TPNode, base_voltage=base_voltage, v_mag=vmag,
                                    base_apparent_power=base_apparent_power, v_phase=vphase,
                                    p=pInj, q=qinj, index=index))
             index = index + 1
         
         #create branches ACLineSegment
         for ACLineSegment in list_ACLineSegment:
-            name_ACLineSegment = ACLineSegment.name
-            start_node_id, end_node_id = self._get_node_names(list_Terminals, name_ACLineSegment)
+            uuid_ACLineSegment = ACLineSegment.mRID
+            start_node_id, end_node_id = self._get_node_uuids(list_Terminals, uuid_ACLineSegment)
             start_node = None
             end_node = None
 
@@ -126,14 +117,14 @@ class System():
                     end_node = node
 
             base_voltage = ACLineSegment.BaseVoltage[0].nominalVoltage
-            self.branches.append(Branch(uuid=name_ACLineSegment, r=ACLineSegment.r, x=ACLineSegment.x, 
+            self.branches.append(Branch(uuid=uuid_ACLineSegment, r=ACLineSegment.r, x=ACLineSegment.x, 
                                         start_node=start_node, end_node=end_node, 
                                         base_voltage=base_voltage, base_apparent_power=base_apparent_power))
 
         #create branches power transformer
         for power_transformer in list_PowerTransformer:
-            name_power_transformer = power_transformer.name
-            start_node_id, end_node_id = self._get_node_names(list_Terminals, name_power_transformer)
+            uuid_power_transformer = power_transformer.mRID
+            start_node_id, end_node_id = self._get_node_uuids(list_Terminals, uuid_power_transformer)
             start_node = None
             end_node = None
 
@@ -144,64 +135,59 @@ class System():
                     end_node = node
 
             # base voltage = high voltage side (=primaryConnection)
-            primary_connection = self._get_primary_connection(list_PowerTransformerEnds, name_power_transformer)
+            primary_connection = self._get_primary_connection(list_PowerTransformerEnds, uuid_power_transformer)
             base_voltage = primary_connection.BaseVoltage[0].nominalVoltage
-            self.branches.append(Branch(uuid=name_power_transformer, r=primary_connection.r, x=primary_connection.x,
+            self.branches.append(Branch(uuid=uuid_power_transformer, r=primary_connection.r, x=primary_connection.x,
                                         start_node=start_node, end_node=end_node, base_voltage=base_voltage,
                                         base_apparent_power=base_apparent_power))
 
         self._setNodeType(list_Terminals)
+        
         # calculate admitance matrix
         self.Ymatrix_calc()
 
-    def _get_node_names(self, list_Terminals, elem_name):
+    def _get_node_uuids(self, list_Terminals, elem_uuid):
         """
-        get the names of the start and end nodes of the element with name=elem_name
+        get the uuids of the start and end nodes of the element with uuid=elem_uuid
         This function can only used with the elements ACLineSegment, PowerTransformer, Switch and EnergyConsumer
         :param list_Terminals: list of all elements of type Terminal
-        :param elem_name: name of the element for which the start and end node ID are searched
+        :param elem_uuid: uuid of the element for which the start and end node ID are searched
         :return tuple: (startNodeID, endNodeID)
         """
-        start_node_name = ""
-        end_node_name = ""
+        start_node_uuid = ""
+        end_node_uuid = ""
         list_elements = ["ACLineSegment", "PowerTransformer", "Switch", "EnergyConsumer"]
         
         for terminal in list_Terminals:
             #TODO: has always the list ConductingEquipment only one element?????
             conductingEquipment = terminal.ConductingEquipment[0]
-            if (conductingEquipment.name != elem_name):
+            if (conductingEquipment.mRID != elem_uuid):
                 continue
             elif conductingEquipment.__class__.__name__ in list_elements:
-                sequenceNumber = terminal.sequenceNumber
-                #TODO: has always the list TopologicalNode only one element?????
-                topologicalNode = terminal.TopologicalNode[0]
-                if sequenceNumber == 1:
-                    start_node_name = topologicalNode.name
-                elif sequenceNumber == 2:
-                    end_node_name = topologicalNode.name
+                sequence_number = terminal.sequenceNumber
+                #TODO: has always the list Terminal.TopologicalNode only one element?????
+                topological_node = terminal.TopologicalNode[0]
+                if sequence_number == 1:
+                    start_node_uuid = topological_node.mRID
+                elif sequence_number == 2:
+                    end_node_uuid = topological_node.name
         
-        return (start_node_name, end_node_name)
+        return (start_node_uuid, end_node_uuid)
 
-    def _get_primary_connection(self, list_PowerTransformerEnds, elem_name):
+    def _get_primary_connection(self, list_PowerTransformerEnds, elem_uuid):
         """
-        get primaryConnection of the powertransformer with name = elem_name
+        get primaryConnection of the powertransformer with uuid = elem_uuid
         :param list_PowerTransformerEnd: list of all elements of type PowerTransformerEnd
-        :param elem_name: name of the power transformer for which the primary connection is searched
+        :param elem_uuid: uuid of the power transformer for which the primary connection is searched
         :return: primary_connection
 	    """
         primary_connection = None
         power_transformer_ends = []
 
-        #search for two elements of class powertransformerend that point to the powertransformer with ID = elem_id
+        #search for two elements of class powertransformerend that point to the powertransformer with ID = elem_uuid
         for power_transformer_end in list_PowerTransformerEnds:
-            if power_transformer_end.PowerTransformer[0].name == elem_name:
+            if power_transformer_end.PowerTransformer[0].mRID == elem_uuid:
                 power_transformer_ends.append(power_transformer_end)
-
-        #TODO: ERROR HANDLING
-        if len(power_transformer_ends) !=2:
-            print("ERROR!!!")
-            print(len(power_transformer_ends))
-            return -1
 
         if power_transformer_ends[0].BaseVoltage[0].nominalVoltage>=power_transformer_ends[1].BaseVoltage[0].nominalVoltage:
             primary_connection=power_transformer_ends[0]
@@ -212,26 +198,27 @@ class System():
 
     def _setNodeType(self, list_Terminals):
         """
-        return the type of a node: PQ, PV or SLACK
-
-        @param node: element of class cimpy.Topology.TopologicalNode
+        set the parameter type of all elements of the list self.nodes
+        :param list_PowerTransformerEnd: list of all elements of type Terminal
+        :return None
         """
         #get a list of Terminals for which the ConductingEquipment is a element of class ExternalNetworkInjection
         list_Terminals_ENI = [elem for elem in list_Terminals if elem.ConductingEquipment[0].__class__.__name__ == "ExternalNetworkInjection"]
         for terminal in list_Terminals_ENI:
             #TODO: is it correct to use the element 0 for it? Is len(terminal.TopologicalNode) greater than one?
-            node_name = terminal.TopologicalNode[0].name
+            node_uuid = terminal.TopologicalNode[0].mRID
             for node in self.nodes:
-                if node.uuid == node_name:
+                if node.uuid == node_uuid:
                     node.type = BusType["SLACK"]
             
-        #get a list of Terminals for which the ConductingEquipment is a element of class ExternalNetworkInjection
+        #TODO the search for PV nodes has not been tested yet
+        #get a list of Terminals for which the ConductingEquipment is a element of class SynchronousMachine
         list_Terminals_SM = [elem for elem in list_Terminals if elem.ConductingEquipment[0].__class__.__name__ == "SynchronousMachine"]
         for terminal in list_Terminals_SM:
             #TODO: is it correct to use the element 0 for it? Is len(terminal.TopologicalNode) greater than one?
-            node_name = terminal.TopologicalNode[0].name
+            node_uuid = terminal.TopologicalNode[0].MRID
             for node in self.nodes:
-                if node.uuid == node_name:
+                if node.uuid == node_uuid:
                     node.type = BusType["PV"]
 
     def Ymatrix_calc(self):
