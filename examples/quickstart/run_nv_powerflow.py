a77d83ea8c3f532dff5e1dbea0deb11f1c66a2b5
Markus Mirz <mmirz@eonerc.rwth-aachen.de>
2020-02-20 20:29:26 +0100

    use pathlib to specify cim files

--- examples/quickstart/run_nv_powerflow.py
+++ examples/quickstart/run_nv_powerflow.py
@@ -1,26 +1,20 @@
 import logging
-from acs.state_estimation import network
-from acs.state_estimation import nv_powerflow
-from acs.state_estimation import results
+from pyvolt import network
+from pyvolt import nv_powerflow
+from pyvolt import results
 import cimpy
 import os
 from villas.dataprocessing.readtools import read_timeseries_dpsim
-
-
-#print(os.path.dirname(__file__))
-os.chdir(os.path.dirname(__file__))
+from pathlib import Path
 
 logging.basicConfig(filename='CIGRE.log', level=logging.INFO, filemode='w')
 
-xml_path = r".\sample_data\CIGRE-MV-NoTap"
-xml_files = [xml_path + r"\Rootnet_FULL_NE_06J16h_DI.xml",
-                 xml_path + r"\Rootnet_FULL_NE_06J16h_EQ.xml",
-                 xml_path + r"\Rootnet_FULL_NE_06J16h_SV.xml",
-                 xml_path + r"\Rootnet_FULL_NE_06J16h_TP.xml"]
-
-xml_files_abs = []
-for file in xml_files:
-    xml_files_abs.append(os.path.abspath(file))
+cim_dir = Path('.') / 'sample_data' / 'CIGRE-MV-NoTap'
+cim_files = cim_dir.glob('*.xml')
+cim_list = []
+for file in cim_files:
+    cim_list.append(str(file.absolute()))
+print(cim_list)
 
 # read cim files and create new network.Systen object
 res, _ = cimpy.cim_import(xml_files_abs, "cgmes_v2_4_15")
