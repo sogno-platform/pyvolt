5c02a5e255c5c7b7af37221a5d717f7b807505ec
ss391347 <sebastian.schwarz1@rwth-aachen.de>
2021-02-17 11:42:10 +0100

    make all pyvolt examples to work with the recent cimpy version (master branch at https://github.com/cim-iec/cimpy)
    Signed-off-by: ss391347 <sebastian.schwarz1@rwth-aachen.de>

--- examples/quickstart/run_nv_powerflow.py
+++ examples/quickstart/run_nv_powerflow.py
@@ -1,26 +1,21 @@
 import logging
 from pyvolt import network
 from pyvolt import nv_powerflow
-from pyvolt import results
 import cimpy
 import os
-from pathlib import Path
 
-logging.basicConfig(filename='CIGRE.log', level=logging.INFO, filemode='w')
 
-this_file_folder = Path(__file__).resolve().parent
-xml_path = this_file_folder / "sample_data" / "CIGRE-MV-NoTap"
-xml_files = [xml_path / "Rootnet_FULL_NE_06J16h_DI.xml",
-             xml_path / "Rootnet_FULL_NE_06J16h_EQ.xml",
-             xml_path / "Rootnet_FULL_NE_06J16h_SV.xml",
-             xml_path / "Rootnet_FULL_NE_06J16h_TP.xml"]
+logging.basicConfig(filename='run_nv_powerflow.log', level=logging.INFO, filemode='w')
 
-xml_files_abs = []
-for file in xml_files:
-    xml_files_abs.append(os.path.abspath(file))
+this_file_folder = os.path.dirname(os.path.realpath(__file__))
+xml_path = os.path.realpath(os.path.join(this_file_folder, "..", "sample_data", "CIGRE-MV-NoTap"))
+xml_files = [os.path.join(xml_path, "Rootnet_FULL_NE_06J16h_DI.xml"),
+             os.path.join(xml_path, "Rootnet_FULL_NE_06J16h_EQ.xml"),
+             os.path.join(xml_path, "Rootnet_FULL_NE_06J16h_SV.xml"),
+             os.path.join(xml_path, "Rootnet_FULL_NE_06J16h_TP.xml")]
 
-# read cim files and create new network.Systen object
-res = cimpy.cim_import(xml_files_abs, "cgmes_v2_4_15")
+# Read cim files and create new network.System object
+res = cimpy.cim_import(xml_files, "cgmes_v2_4_15")
 system = network.System()
 base_apparent_power = 25  # MW
 system.load_cim_data(res['topology'], base_apparent_power)
@@ -28,7 +23,7 @@ system.load_cim_data(res['topology'], base_apparent_power)
 # Execute power flow analysis
 results_pf, num_iter = nv_powerflow.solve(system)
 
-# print node voltages
+# Print node voltages
 print("Powerflow converged in " + str(num_iter) + " iterations.\n")
 print("Results: \n")
 voltages = []
@@ -37,5 +32,14 @@ for node in results_pf.nodes:
     #print('{}={}'.format(node.topology_node.uuid, node.voltage))
     voltages.append(node.voltage_pu)
 
-voltages_ref = [(1-7.970485900477431e-27j), (0.9521818868802214-0.11692768153747995j), (0.9642955926931457-0.09862127081290231j), (0.8796973782245792-0.15318580971335868j), (0.8758799767843979-0.1554670528853566j), (0.8774339089327876-0.1545390713879984j), (0.8704521134131005-0.1574589214738466j), (0.8719578342107204-0.15661905676450852j), (0.8731024990161087-0.1561497673751958j), (0.8727417602545003-0.15623359013901236j), (0.8740410220986286-0.1565630049874985j), (0.9563016474701451-0.09917826765833906j), (0.9592141716734833-0.09896267637101246j), (0.8702137462858025-0.15760036065945185j), (0.9239489705253996-0.13105032262255972j)]
-assert (voltages == voltages_ref), "Results do not match reference results." 
+voltages_ref = [(1-7.970485900477431e-27j), (0.9521818868802214-0.11692768153747995j),
+                (0.9642955926931457-0.09862127081290231j), (0.8796973782245792-0.15318580971335868j),
+                (0.8758799767843979-0.1554670528853566j), (0.8774339089327876-0.1545390713879984j),
+                (0.8704521134131005-0.1574589214738466j), (0.8719578342107204-0.15661905676450852j),
+                (0.8731024990161087-0.1561497673751958j), (0.8727417602545003-0.15623359013901236j),
+                (0.8740410220986286-0.1565630049874985j), (0.9563016474701451-0.09917826765833906j),
+                (0.9592141716734833-0.09896267637101246j), (0.8702137462858025-0.15760036065945185j),
+                (0.9239489705253996-0.13105032262255972j)]
+epsilon = 1e-4
+assert ([val-epsilon for val in voltages] <= voltages_ref <= [val+epsilon for val in voltages]), \
+    "Results do not match reference results."
--- examples/quickstart/test_switch_nv_powerflow.py
+++ examples/quickstart/test_switch_nv_powerflow.py
@@ -2,35 +2,32 @@ import logging
 import numpy as np
 from pyvolt import network
 from pyvolt import nv_powerflow
-from pyvolt import results
 import cimpy
 import os
 
-logging.basicConfig(filename='CIGRE.log', level=logging.INFO, filemode='w')
 
-xml_path = r".\sample_data\CIGRE-MV-NoTap-WithBreaker"
-xml_files = [xml_path + r"\20191126T1535Z_YYY_EQ_.xml",
-             xml_path + r"\20191126T1535Z_XX_YYY_SV_.xml",
-             xml_path + r"\20191126T1535Z_XX_YYY_TP_.xml"]
+logging.basicConfig(filename='test_switch_nv_powerflow.log', level=logging.INFO, filemode='w')
 
-xml_files_abs = []
-for file in xml_files:
-    xml_files_abs.append(os.path.abspath(file))
+this_file_folder = os.path.dirname(os.path.realpath(__file__))
+xml_path = os.path.realpath(os.path.join(this_file_folder, "..", "sample_data", "CIGRE-MV-NoTap-WithBreaker"))
+xml_files = [os.path.join(xml_path, "20191126T1535Z_YYY_EQ_.xml"),
+             os.path.join(xml_path, "20191126T1535Z_XX_YYY_SV_.xml"),
+             os.path.join(xml_path, "20191126T1535Z_XX_YYY_TP_.xml")]
 
-# read cim files and create new network.Systen object
-res, _ = cimpy.cim_import(xml_files_abs, "cgmes_v2_4_15")
+# Read cim files and create new network.System object
+res = cimpy.cim_import(xml_files, "cgmes_v2_4_15")
 system = network.System()
 base_apparent_power = 25  # MW
-system.load_cim_data(res, base_apparent_power)
+system.load_cim_data(res['topology'], base_apparent_power)
 
-#open breaker
+# Open breaker
 system.breakers[-1].open_breaker()
 system.Ymatrix_calc()
 
 # Execute power flow analysis
 results_pf, num_iter = nv_powerflow.solve(system)
 
-# print node voltages
+# Print node voltages
 print("Powerflow converged in " + str(num_iter) + " iterations.\n")
 print("Results:")
 for node in results_pf.nodes:
@@ -41,14 +38,14 @@ print("\n")
 print("-------------------------------------------------------------")
 print("\n")
 
-#close breaker
+# Close breaker
 system.breakers[-1].close_breaker()
 system.Ymatrix_calc()
 
 # Execute power flow analysis
 results_pf, num_iter = nv_powerflow.solve(system)
 
-# print node voltages
+# Print node voltages
 print("Powerflow converged in " + str(num_iter) + " iterations.\n")
 print("Results:")
 for node in results_pf.nodes:
